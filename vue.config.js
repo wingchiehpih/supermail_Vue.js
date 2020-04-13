629568d02e10e4c280b6817250b113461fed1e02
wingchiehpih <wingchiehpih@gmail.com>
2020-04-13 23:41:12 +0800

    项目模块划分

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,16 @@
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
 module.exports = {
-  configureWebpack: {
-    // 解决路径相关问题
-    resolve: {
-      // 配置别名
-      alias: {
-        assets: 'src/assets',
-        common: 'src/common',
-        components: 'src/components',
-        network: 'src/network',
-        views: 'src/views'
-      }
-    }
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("@", resolve("./src"))
+      .set("assets", resolve("./src/assets"))
+      .set("common", resolve("./src/common"))
+      .set("components", resolve("./src/components"))
+      .set("network", resolve("./src/network"))
+      .set("views", resolve("./src/views"));
   }
 };
